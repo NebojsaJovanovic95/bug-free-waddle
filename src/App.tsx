6cf13e6eed755bd99312d0930aaf64023efb1579
Nebojsa Jovanovic <60476402+NebojsaJovanovic95@users.noreply.github.com>
2023-02-27 02:09:04 +0000

    deployed to pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,10 @@ import * as React from "react"
 import {
     ChakraProvider,
     Box,
-    Text,
-    Link,
-    VStack,
-    Code,
-    Grid,
     theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+//import { ColorModeSwitcher } from "./ColorModeSwitcher"
+//import { Logo } from "./Logo"
 import KanbanBoard from "./components/KanbanBoard"
 
 export const App = () => (
